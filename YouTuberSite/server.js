63d5e3f14ab5cf8c6f35e3231ffaed3e85dcf283
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-08-12 20:23:13 +0100

    began setting up project

--- YouTuberSite/server.js
+++ YouTuberSite/server.js
@@ -1,5 +1,21 @@
 ﻿var express = require('express');
+var body_parser = require('body-parser');
+var swig = require('swig');
+
 var app = express();
+
+//Configure templating
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/templates');
+
+//Allows static files
+app.use(express.static('public'));
+
+//Refactor requests
+app.use(body_parser.urlencoded({
+	extended: false
+}));
 
 app.get('/', function (req, res) {
 	res.send('A work in prgoress!');
