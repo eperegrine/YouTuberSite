ad225fc07dd57cb7b5238d5214910562b608d9f3
Tom <tomfperegrine@googlemail.com>
2015-08-16 01:43:52 +0100

    Completed load more functionality

--- YouTuberSite/public/js/load-videos.js
+++ YouTuberSite/public/js/load-videos.js
@@ -15,6 +15,8 @@ function load_more () {
 	if (token) {
 		jQuery.get(url + "?pt=" + token, function(data) {
 			console.log(data);
+
+			//Handle the page token for load more button
 			var nextPage = data.nextPageToken;
 			console.log(data.nextPageToken);
 			if (nextPage) {
@@ -22,6 +24,22 @@ function load_more () {
 			} else {
 				load_btn.hide('slow');
 			}
+
+			//loop through the videos and generate html for them
+			var videos = [];
+
+			for (var i = 0; i < data.items.length; i++) {
+	          video = generate_video(data.items[i]);
+	          if (video) {
+	            videos.push(video);
+	          }
+	        }
+
+	        for (var i = 0; i < videos.length; i++) {
+	        	var html = generate_vid_html(videos[i]);
+	        	$("#videoList").append(html);
+	        	//.find('a:last-child').hide().fadeIn('slow');
+	        }
 		});
 	} else {
 		console.log('load_btn has no attribute next-page')
@@ -38,4 +56,11 @@ function generate_video (vidJson) {
   } else {
     return false;
   }
+}
+function generate_vid_html (video) {
+	var htmlStr = '<a href="' + 
+	video.url + '"> <li class="vid-item"> <h3>'+ 
+	video.name + '</h3> <img width="100%" src="' + 
+	video.thumbnailUrl + '"></img> </li></a>';
+    return htmlStr;
 }
